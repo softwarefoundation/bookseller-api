c7973a747928ecdc6991619af04a27a96a923aff
Dherkyan Ribeiro <dherkyan@gmail.com>
2023-05-13 09:12:18 -0300

    Project Book Seller API

---
+++ src/main/java/com/softwarefoundation/booksellerapi/BooksellerApiApplication.java
@@ -0,0 +1,13 @@
+package com.softwarefoundation.booksellerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksellerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksellerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwarefoundation/booksellerapi/BooksellerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarefoundation.booksellerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksellerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
