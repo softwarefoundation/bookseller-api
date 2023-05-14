1755ee68c41b74855e729992e1a6b0ab9cdd763d
Dherkyan Ribeiro <dherkyan@gmail.com>
2023-05-14 02:48:53 -0300

    [Aula-40] JWT Helper Methods - Token Generation, Validation

---
+++ src/main/java/com/softwarefoundation/booksellerapi/security/jwt/IJwtProvider.java
@@ -0,0 +1,14 @@
+package com.softwarefoundation.booksellerapi.security.jwt;
+
+import com.softwarefoundation.booksellerapi.security.UserPrincipal;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface IJwtProvider {
+    String generateToken(UserPrincipal auth);
+
+    Authentication getAuthentication(HttpServletRequest request);
+
+    boolean validateToken(HttpServletRequest request);
+}
--- src/main/java/com/softwarefoundation/booksellerapi/util/SecurityUtils.java
+++ src/main/java/com/softwarefoundation/booksellerapi/util/SecurityUtils.java
@@ -2,17 +2,31 @@ package com.softwarefoundation.booksellerapi.util;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 public class SecurityUtils {
 
     public static final String ROLE_PREFIX = "ROLE_";
+    public static final String AUTH_HEADER = "authorization";
+    public static final String AUTH_TOKEN_TYPE = "Bearer";
+    public static final String AUTH_TOKEN_PREFIX = AUTH_TOKEN_TYPE.concat(" ");
 
     public static SimpleGrantedAuthority convertToAuthority(String role) {
         log.info("TRACKER: SimpleGrantedAuthority: {0}", role);
         String formattedRole = role.startsWith(ROLE_PREFIX) ? role : ROLE_PREFIX.concat(role);
         log.info("TRACKER: SimpleGrantedAuthority formattedRole: {0}", formattedRole);
         return new SimpleGrantedAuthority(formattedRole);
+    }
+
+    public static String extractAuthTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTH_HEADER);
+        if (!StringUtils.hasLength(bearerToken) && bearerToken.startsWith(AUTH_TOKEN_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
     }
 
 
